d1171206202ed1db5b3117e5e91b8dfbb224d1cf
m.flueh <mf@esyoil.com>
2018-03-12 14:09:22 +0100

    add input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
+
+  state = {
+    input: ''
+  };
+
+  onChangeHandler = (event) => {
+    this.setState(
+      {
+        input: event.target.value
+      }
+    )
+  };
+
   render() {
     return (
-      
+      <div>
+        <h1>New Application</h1>
+        <input onChange={this.onChangeHandler}/>
+        <p>{this.state.input.length}</p>
+      </div>
     );
   }
 }
---
+++ src/Validation/Validation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const validation = (props) => {
+  return (
+    
+  )
+};
+
+export default validation;
